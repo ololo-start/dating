fa8001902bd170a3cb9c594409bd3337d4501638
olga-ploloo <olga.kozintsava@gmail.com>
2020-05-31 17:34:45 +0300

    Set

--- dating/settings.py
+++ dating/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'registration',
     'chat',
     'social_django',
-    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -130,15 +129,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), os.path.join(BASE_DIR, 'media')]
 
 MEDIA_URL = '/media/'
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
