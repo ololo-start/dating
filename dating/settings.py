94babcb831d0c739c61593fed41d95b1e22b8545
olga-ploloo <olga.kozintsava@gmail.com>
2020-05-31 16:37:33 +0300

    Set

--- dating/settings.py
+++ dating/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), os.path.join(BASE_DIR, 'media')]
-
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
