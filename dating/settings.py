438d46b67c4fce968e76081c991fadd9ce27c36f
olga-ploloo <olga.kozintsava@gmail.com>
2020-06-01 12:22:38 +0300

    Set

--- dating/settings.py
+++ dating/settings.py
@@ -135,7 +135,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_FINDERS = [
