e0d23f396048c68962371401cf41d38b85127bdc
olga-ploloo <olga.kozintsava@gmail.com>
2020-06-01 11:29:21 +0300

    Set

--- chat/urls.py
+++ chat/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from chat import views
 from chat.views import MessagesView, CreateDialogView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-
-
     path('chat', views.get_all_chats, name='chats'),
     path('chat/create/<int:user_id>', CreateDialogView.as_view(),  name='create_chat'),
     path('chat/<int:pk>', MessagesView.as_view(), name='messages')
