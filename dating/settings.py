611e527734de7d905f87e2194c6167b1af94ecb7
olga-ploloo <olga.kozintsava@gmail.com>
2020-06-01 14:33:45 +0300

    Set

--- dating/settings.py
+++ dating/settings.py
@@ -131,14 +131,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'live-static', 'static-root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'media')
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'live-static', 'media-root')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'media')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_FINDERS = [
